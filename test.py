d1b8d67ec6355063cfd7d1472fdd3f0596b8f4ff
Mario-dango <mario.dango@users.noreply.github.com>
2021-04-17 21:38:36 -0300

    Implementación de reconocimiento de voz con google

--- test.py
+++ test.py
@@ -18,9 +18,34 @@ time_()
 def inicio():
      hora = datetime.datetime.now().hour
 
-     if hora >= 6 and hora < 12:
+    if hora>=6 and hora<12:
         hablar("Buenos días señor")
-    elif hora >= 12 and hora < 18:
+    elif hora>=12 and hora<18:
         hablar("Buenas tardes señor")
-    elif hora >= 18 and hora <24
+    #elif hora >= 18 and hora <24:
+    else:
         hablar("Buenas noches señor")
+
+    hablar("DangoProtype a su servicio")
+
+def TomarComando():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Escuchando.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    
+    try:
+        print("Reconociendo.....")
+        query = r.recognize_google(audio, language='es')
+        print(query)
+
+    except Exception as e:
+        print(e)
+        print("Digalo nuevamente, porfavor")
+        hablar("Digalo nuevamente, porfavor")
+        return "None"
+
+    return query
+
+TomarComando()
